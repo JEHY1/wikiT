9078c5d2769e2348d148c50201c7f8bd60455461
Kim Jeahyeon <poiuy015201@gmail.com>
2024-03-27 17:48:41 +0900

    0.0.2

--- demo/src/main/java/com/wikiT/demo/controller/ArticleViewController.java
+++ demo/src/main/java/com/wikiT/demo/controller/ArticleViewController.java
@@ -1,9 +1,12 @@
 package com.wikiT.demo.controller;
 
 import com.wikiT.demo.domain.Article;
+import com.wikiT.demo.domain.GroupMaker;
 import com.wikiT.demo.dto.ArticleStatusViewRequest;
 import com.wikiT.demo.dto.ArticleStatusViewResponse;
+import com.wikiT.demo.repository.GroupMakerRepository;
 import com.wikiT.demo.service.ArticleService;
+import com.wikiT.demo.service.GroupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +22,7 @@ import java.security.Principal;
 public class ArticleViewController {
 
     private final ArticleService articleService;
+    private final GroupService groupService;
 
     @GetMapping("/newArticle")
     public String showNewArticleView(ArticleStatusViewRequest request, Principal principal, Model model){
@@ -26,6 +30,7 @@ public class ArticleViewController {
         System.err.println("space : " + request.getSpace());
 
         if(request.getArticleId() == null){
+            model.addAttribute("groupName", groupService.findGroupName(request.getGroupId()));
             System.err.println("articleId : " + request.getArticleId());
             model.addAttribute("article", new ArticleStatusViewResponse(request.getGroupId(), request.getSpace(), principal.getName()));
             return "newArticle";
@@ -34,6 +39,10 @@ public class ArticleViewController {
         Article article = articleService.findById(request.getArticleId());
         model.addAttribute("article", new ArticleStatusViewResponse(article));
         model.addAttribute("myEmail", principal.getName());
+        model.addAttribute("groupName", groupService.findGroupName(request.getGroupId()));
+        model.addAttribute("constructor", groupService.findConstructor(request.getGroupId()));
+
+
 
         return "newArticle";
     }
--- demo/src/main/java/com/wikiT/demo/dto/ArticleListViewResponse.java
+++ demo/src/main/java/com/wikiT/demo/dto/ArticleListViewResponse.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ArticleListViewResponse {
-    private final int SUMMARY_SIZE = 50;
+    private final int SUMMARY_SIZE = 80;
 
     private Long articleId;
     private String title;
